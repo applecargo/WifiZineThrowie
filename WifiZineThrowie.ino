055adbf61207dd00bf67e6897efab76f30ecbecf
Dooho Yi <pilotedeguerre@gmail.com>
2019-01-07 17:21:05 +0900

    init repo with last final version of wifi-zine (2017 winter)
    - excluding many parts like: hardware/, libraries/WebServer_tng, tools/ESP32FS
    - exclusion is due to license issues.. I don't really want to deal with it seriously, since my prefered license is Unlicense.
    - but, well. my code is a modification of FSBrowser.ino of arduino example.. which is in lgpl-2.1. so, let it be what it should be. bye2 unlicense.
    - added an archive.rst file, which is the result of ls -R
    - this might give some hints about the full set of codes that was being used for the pre-existing wifizine
    - for example, which version of 'xtensa-esp32-elf' was used, etc.
    - at that time, that mattered.. to make this compile and work properly.
    
    - anyway we will update this to be sth. more contemporary.
    - on the way, will apply necessary licenses as needed..
    
    Signed-off-by: Dooho Yi <pilotedeguerre@gmail.com>

---
+++ WifiZineThrowie.ino
@@ -0,0 +1,373 @@
+/* 
+  FSWebServer - Example WebServer with SPIFFS backend for esp8266
+  Copyright (c) 2015 Hristo Gochkov. All rights reserved.
+  This file is part of the ESP8266/ESP32 WebServer library for Arduino environment.
+ 
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+  
+  upload the contents of the data folder with MkSPIFFS Tool ("ESP8266 Sketch Data Upload" in Tools menu in Arduino IDE)
+  or you can upload the contents of a folder if you CD in that folder and run the following command:
+  for file in `ls -A1`; do curl -F "file=@$PWD/$file" esp8266fs.local/edit; done
+  
+  access the sample web page at http://esp8266fs.local
+  edit the page by going to http://esp8266fs.local/edit
+*/
+
+#define CAPTIVE_PORTAL
+const char *softAP_ssid = "오! 와이파이 ZINE?";
+const char *myHostname = "esp32";
+
+/*
+  Captive portal mode does not connect to the Internet.
+  If you want to get access to the Internet, comment above #define CAPTIVE_PORTAL and fill out following ssid/passwd.
+  Having internet might help when you do utilize 'edit' page, since that page uses CDNs a lot.
+  But, in my expriences, it was working fine somehow even without the Internet!
+
+  Edit page is available at "http://192.168.4.1/edit"
+  After editing, save your edit by pressing Ctrl(Cmd)-S !!
+*/
+
+const char* ssid = "nosignal";
+const char* password = "1111100000";
+const char* host = "esp32"; // when the server is in NON captive portal mode, mDNS .local works..
+
+//
+
+#include <ESPmDNS.h>
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <WebServer.h>
+#include <SPIFFS.h>
+WebServer server(80);
+
+#include "DNSServer.h"
+const byte DNS_PORT = 53;
+IPAddress apIP(192, 168, 4, 1);
+IPAddress netMsk(255, 255, 255, 0);
+DNSServer dnsServer;
+
+#define DBG_OUTPUT_PORT Serial
+
+//holds the current upload
+File fsUploadFile;
+
+//format bytes
+String formatBytes(size_t bytes){
+  if (bytes < 1024){
+    return String(bytes)+"B";
+  } else if(bytes < (1024 * 1024)){
+    return String(bytes/1024.0)+"KB";
+  } else if(bytes < (1024 * 1024 * 1024)){
+    return String(bytes/1024.0/1024.0)+"MB";
+  } else {
+    return String(bytes/1024.0/1024.0/1024.0)+"GB";
+  }
+}
+
+String getContentType(String filename){
+  if(server.hasArg("download")) return "application/octet-stream";
+  else if(filename.endsWith(".htm")) return "text/html";
+  else if(filename.endsWith(".html")) return "text/html";
+  else if(filename.endsWith(".css")) return "text/css";
+  else if(filename.endsWith(".js")) return "application/javascript";
+  else if(filename.endsWith(".png")) return "image/png";
+  else if(filename.endsWith(".gif")) return "image/gif";
+  else if(filename.endsWith(".jpg")) return "image/jpeg";
+  else if(filename.endsWith(".ico")) return "image/x-icon";
+  else if(filename.endsWith(".xml")) return "text/xml";
+  else if(filename.endsWith(".pdf")) return "application/x-pdf";
+  else if(filename.endsWith(".zip")) return "application/x-zip";
+  else if(filename.endsWith(".gz")) return "application/x-gzip";
+  return "text/plain";
+}
+
+boolean isIp(String str) {
+  for (int i = 0; i < str.length(); i++) {
+    int c = str.charAt(i);
+    if (c != '.' && (c < '0' || c > '9')) {
+      return false;
+    }
+  }
+  return true;
+}
+
+String toStringIp(IPAddress ip) {
+  String res = "";
+  for (int i = 0; i < 3; i++) {
+    res += String((ip >> (8 * i)) & 0xFF) + ".";
+  }
+  res += String(((ip >> 8 * 3)) & 0xFF);
+  return res;
+}
+
+// Redirect to captive portal if we got a request for another domain. Return true in that case so the page handler do not try to handle the request again.
+boolean captivePortal() {
+  //DBG_OUTPUT_PORT.println(server.hostHeader());
+  if (!isIp(server.hostHeader()) && server.hostHeader() != (String(myHostname)+".local")) {
+    DBG_OUTPUT_PORT.println("Request redirected to captive portal");
+    server.sendHeader("Location", String("http://") + toStringIp(server.client().localIP()), true);
+    server.send ( 302, "text/plain", ""); // Empty content inhibits Content-length header so we have to close the socket ourselves.
+    server.client().stop(); // Stop is needed because we sent no content length
+    return true;
+  }
+  return false;
+}
+
+bool handleFileRead(String path){
+  if (captivePortal()) { // If captive portal redirect instead of displaying the page.
+    return true;
+  }
+  DBG_OUTPUT_PORT.println("handleFileRead: " + path);
+  if(path.endsWith("/")) path += "index.htm";
+  String contentType = getContentType(path);
+  String pathWithGz = path + ".gz";
+  if(SPIFFS.exists(pathWithGz) || SPIFFS.exists(path)){
+    if(SPIFFS.exists(pathWithGz))
+      path += ".gz";
+    File file = SPIFFS.open(path, "r");
+    size_t sent = server.streamFile(file, contentType);
+    file.close();
+    return true;
+  }
+  return false;
+}
+
+void handleFileUpload(){
+  if (captivePortal()) { // If captive portal redirect instead of displaying the page.
+    return;
+  }
+  if(server.uri() != "/edit") return;
+  HTTPUpload& upload = server.upload();
+  if(upload.status == UPLOAD_FILE_START){
+    String filename = upload.filename;
+    if(!filename.startsWith("/")) filename = "/"+filename;
+    DBG_OUTPUT_PORT.print("handleFileUpload Name: "); DBG_OUTPUT_PORT.println(filename);
+    fsUploadFile = SPIFFS.open(filename, "w");
+    filename = String();
+  } else if(upload.status == UPLOAD_FILE_WRITE){
+    //DBG_OUTPUT_PORT.print("handleFileUpload Data: "); DBG_OUTPUT_PORT.println(upload.currentSize);
+    if(fsUploadFile)
+      fsUploadFile.write(upload.buf, upload.currentSize);
+  } else if(upload.status == UPLOAD_FILE_END){
+    if(fsUploadFile)
+      fsUploadFile.close();
+    DBG_OUTPUT_PORT.print("handleFileUpload Size: "); DBG_OUTPUT_PORT.println(upload.totalSize);
+  }
+}
+
+void handleFileDelete(){
+  if (captivePortal()) { // If captive portal redirect instead of displaying the page.
+    return;
+  }
+  if(server.args() == 0) return server.send(500, "text/plain", "BAD ARGS");
+  String path = server.arg(0);
+  DBG_OUTPUT_PORT.println("handleFileDelete: " + path);
+  if(path == "/")
+    return server.send(500, "text/plain", "BAD PATH");
+  if(!SPIFFS.exists(path))
+    return server.send(404, "text/plain", "FileNotFound");
+  SPIFFS.remove(path);
+  server.send(200, "text/plain", "");
+  path = String();
+}
+
+void handleFileCreate(){
+  if (captivePortal()) { // If captive portal redirect instead of displaying the page.
+    return;
+  }
+  if(server.args() == 0)
+    return server.send(500, "text/plain", "BAD ARGS");
+  String path = server.arg(0);
+  DBG_OUTPUT_PORT.println("handleFileCreate: " + path);
+  if(path == "/")
+    return server.send(500, "text/plain", "BAD PATH");
+  if(SPIFFS.exists(path))
+    return server.send(500, "text/plain", "FILE EXISTS");
+  File file = SPIFFS.open(path, "w");
+  if(file)
+    file.close();
+  else
+    return server.send(500, "text/plain", "CREATE FAILED");
+  server.send(200, "text/plain", "");
+  path = String();
+}
+
+void returnFail(String msg) {
+  server.send(500, "text/plain", msg + "\r\n");
+}
+
+void handleFileList() {
+  if (captivePortal()) { // If captive portal redirect instead of displaying the error page.
+    return;
+  }
+  if(!server.hasArg("dir")) {
+    returnFail("BAD ARGS");
+    return;
+  }
+  String path = server.arg("dir");
+  if(path != "/" && !SPIFFS.exists((char *)path.c_str())) {
+    returnFail("BAD PATH");
+    return;
+  }
+  File dir = SPIFFS.open((char *)path.c_str());
+  path = String();
+  if(!dir.isDirectory()){
+    dir.close();
+    returnFail("NOT DIR");
+    return;
+  }
+  dir.rewindDirectory();
+
+  String output = "[";
+  for (int cnt = 0; true; ++cnt) {
+    File entry = dir.openNextFile();
+    if (!entry)
+    break;
+
+    if (cnt > 0)
+      output += ',';
+
+    output += "{\"type\":\"";
+    output += (entry.isDirectory()) ? "dir" : "file";
+    output += "\",\"name\":\"";
+    // Ignore '/' prefix
+    output += entry.name()+1;
+    output += "\"";
+    output += "}";
+    entry.close();
+  }
+  output += "]";
+  server.send(200, "text/json", output);
+  dir.close();
+}
+
+void listDir(fs::FS &fs, const char * dirname, uint8_t levels) {
+  DBG_OUTPUT_PORT.printf("Listing directory: %s\n", dirname);
+
+  File root = fs.open(dirname);
+  if (!root) {
+    DBG_OUTPUT_PORT.println("Failed to open directory");
+    return;
+  }
+  if (!root.isDirectory()) {
+    DBG_OUTPUT_PORT.println("Not a directory");
+    return;
+  }
+
+  File file = root.openNextFile();
+  while (file) {
+    if (file.isDirectory()) {
+      DBG_OUTPUT_PORT.print("  DIR : ");
+      DBG_OUTPUT_PORT.println(file.name());
+      if (levels) {
+        listDir(fs, file.name(), levels - 1);
+      }
+    } else {
+      DBG_OUTPUT_PORT.print("  FILE: ");
+      DBG_OUTPUT_PORT.print(file.name());
+      DBG_OUTPUT_PORT.print("  SIZE: ");
+      DBG_OUTPUT_PORT.println(file.size());
+    }
+    file = root.openNextFile();
+  }
+}
+
+void setup(void){
+  DBG_OUTPUT_PORT.begin(115200);
+  DBG_OUTPUT_PORT.print("\n");
+  DBG_OUTPUT_PORT.setDebugOutput(true);
+  
+#ifdef CAPTIVE_PORTAL
+  //WIFI INIT
+  //ESP32 -> Captive Portal
+  WiFi.mode(WIFI_AP);
+  WiFi.softAPConfig(apIP, apIP, netMsk);
+  WiFi.softAP(softAP_ssid);
+  delay(500); // Without delay I've seen the IP address blank
+  DBG_OUTPUT_PORT.print("AP IP address: ");
+  DBG_OUTPUT_PORT.println(WiFi.softAPIP());
+
+  /* Setup the DNS server redirecting all the domains to the apIP */  
+  dnsServer.setErrorReplyCode(DNSReplyCode::NoError);
+  dnsServer.start(DNS_PORT, "*", apIP);
+#else
+  //WIFI INIT
+  DBG_OUTPUT_PORT.printf("Connecting to %s\n", ssid);
+  if (String(WiFi.SSID()) != String(ssid)) {
+    WiFi.begin(ssid, password);
+  }
+  
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    DBG_OUTPUT_PORT.print(".");
+  }
+  DBG_OUTPUT_PORT.println("");
+  DBG_OUTPUT_PORT.print("Connected! IP address: ");
+  DBG_OUTPUT_PORT.println(WiFi.localIP());
+
+  MDNS.begin(host);
+  DBG_OUTPUT_PORT.print("Open http://");
+  DBG_OUTPUT_PORT.print(host);
+  DBG_OUTPUT_PORT.println(".local/edit to see the file browser");
+#endif
+
+  //FS INIT
+  SPIFFS.begin();
+  {
+    listDir(SPIFFS, "/", 0);
+    DBG_OUTPUT_PORT.printf("\n");
+  }
+  
+  //SERVER INIT
+  //list directory
+  server.on("/list", HTTP_GET, handleFileList);
+  //load editor
+  server.on("/edit", HTTP_GET, [](){
+    if(!handleFileRead("/edit.htm")) server.send(404, "text/plain", "FileNotFound");
+  });
+  //create file
+  server.on("/edit", HTTP_PUT, handleFileCreate);
+  //delete file
+  server.on("/edit", HTTP_DELETE, handleFileDelete);
+  //first callback is called after the request has ended with all parsed arguments
+  //second callback handles file uploads at that location
+  server.on("/edit", HTTP_POST, [](){ server.send(200, "text/plain", ""); }, handleFileUpload);
+  // server.on("/generate_204", handleFileRead("/"));  //Android captive portal. Maybe not needed. Might be handled by notFound handler.
+  // server.on("/fwlink", handleFileRead(String("/")));  //Microsoft captive portal. Maybe not needed. Might be handled by notFound handler.
+
+  //called when the url is not defined here
+  //use it to load content from SPIFFS
+  server.onNotFound([](){
+    if(!handleFileRead(server.uri()))
+      server.send(404, "text/plain", "FileNotFound");
+  });
+
+  //get heap status, analog input value and all GPIO statuses in one json call
+  server.on("/all", HTTP_GET, [](){
+    String json = "{";
+    json += "\"heap\":"+String(ESP.getFreeHeap());
+    json += ", \"analog\":"+String(analogRead(A0));
+    json += "}";
+    server.send(200, "text/json", json);
+    json = String();
+  });
+
+  //
+  server.begin();
+  DBG_OUTPUT_PORT.println("HTTP server started");
+}
+ 
+void loop(void){
+  dnsServer.processNextRequest();
+  server.handleClient();
+}
